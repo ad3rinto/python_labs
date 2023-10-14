628569a443e00b78455e6e1ff85825de25531608
ad3rinto <aad3rinto@gmail.com>
2023-10-14 20:50:27 +0100

    Solved first part of blackjacks

--- spyder workbook.py
+++ spyder workbook.py
@@ -178,14 +178,36 @@ while should_continue:
 
  '''
 #CAPSTONE PROJECT 15 OCT 2023
+import random
+
+player_cards = [random.randint(1,11),random.randint(1,11) ]
+print(f"Your cards: {player_cards}")   
+computer_card1 = random.randint(1,11)    
+print(f"Computer's first card: {computer_card1}")      
+get_more_card = input("Do you want to draw another card? 'y' to get another 'n' to pass\n").lower()  
 
 
+if get_more_card == "n":
+    computer_cards = [computer_card1, random.randint(1, 11)]
+    player_total = 0
+    computer_total = 0
+    for card in player_cards:
+        player_total += card
+    for card in computer_cards:
+        computer_total += card
     
-    
-          
-  
-    
-    
+    print(f"Your final hand :{player_cards}")
+    print(f"Computer's final hand :{computer_cards}")
+    if player_total > computer_total:
+        print("You win")
+    else:
+        print("Computer wins")
+        
+        
+        
+        
+        
+        
     
     
     
